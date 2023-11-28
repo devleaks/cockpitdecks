f10c5fcfb200e2cc76a7c456141b4560525191d8
Pierre M <pierre@devleaks.be>
2023-11-28 17:52:24 +0100

    fcu works perfectly

--- cockpitdecks/buttons/representation/xp_fcu.py
+++ cockpitdecks/buttons/representation/xp_fcu.py
@@ -23,7 +23,7 @@ FCU_DATAREFS = {
     "vnav_managed": "AirbusFBW/ALTmanaged",
     "mach": "sim/cockpit/autopilot/airspeed_is_mach",
     "track": "AirbusFBW/HDGTRKmode",
-    "metricalt": "AirbusFBW/MetricAlt",
+    "vsdashed": "AirbusFBW/VSdashed",
 }
 
 
@@ -57,7 +57,6 @@ class FCUIcon(DrawBase):
 
         # demo through default values
         #
-        metric_alt = self.button.get_dataref_value_from_collection("AirbusFBW/MetricAlt", FCU_COLLECTION, default=0) == 1
         mach_mode = self.button.get_dataref_value_from_collection("sim/cockpit/autopilot/airspeed_is_mach", FCU_COLLECTION, default=0) == 1
         heading_mode = self.button.get_dataref_value_from_collection("AirbusFBW/HDGTRKmode", FCU_COLLECTION, default=1) == 0
 
@@ -81,37 +80,6 @@ class FCUIcon(DrawBase):
         #     self.button.get_dataref_value_from_collection("sim/cockpit/autopilot/vertical_velocity", FCU_COLLECTION),
         # )
 
-        spdft = 0.56 if mach_mode else 249
-        speed_val = self.button.get_dataref_value_from_collection("sim/cockpit2/autopilot/airspeed_dial_kts_mach", FCU_COLLECTION, default=spdft)
-        speed = ""
-        if mach_mode:
-            speed_val = round(speed_val * 100) / 100
-            speed = f"{speed_val:4.2f}"
-        else:
-            speed_val = int(speed_val)
-            speed = f"{speed_val:3d}"
-
-        heading_val = self.button.get_dataref_value_from_collection("sim/cockpit/autopilot/heading_mag", FCU_COLLECTION, 0)
-        heading_val = int(heading_val)
-        heading = f"{heading_val:3d}"
-
-        alt_ft_val = self.button.get_dataref_value_from_collection("sim/cockpit2/autopilot/altitude_dial_ft", FCU_COLLECTION, 26789)
-        alt_ft_val = int(alt_ft_val)
-        alt = f"{alt_ft_val: 5d}"
-
-        # Vertical speed/slope is tricky
-        vsdft = -1200 if heading_mode else -2.5
-        vs_val = self.button.get_dataref_value_from_collection("sim/cockpit/autopilot/vertical_velocity", FCU_COLLECTION, default=vsdft)
-        vs_val_abs = abs(vs_val)
-        vs = ""
-        if vs_val_abs > 99:  # not heading_mode. # this is a hack
-            vs_val_abs = abs(int(vs_val))
-            vs = "-----" if alt_managed else f"{vs_val_abs:4d}".replace("00", "oo")
-        else:
-            vs_val_abs = abs(round(vs_val * 100) / 100)
-            vs = "--.---" if alt_managed else f"{vs_val_abs:4.2f}"
-        # print(">>>", vs_val, heading_mode, alt_managed, vs)
-
         # static texts
         font = self.get_font(text_font, text_size)
         h = text_size + inside
@@ -123,27 +91,27 @@ class FCUIcon(DrawBase):
         draw.text((720, h), text="LAT", font=font, anchor="ls", align="left", fill=text_color)
         if heading_mode:
             draw.text((460, h), text="HDG", font=font, anchor="ls", align="left", fill=text_color)
-            draw.text((980, 120), text="HDG", font=font, anchor="rs", align="right", fill=text_color)
+            draw.text((960, 120), text="HDG", font=font, anchor="rs", align="right", fill=text_color)
         else:
             draw.text((590, h), text="TRK", font=font, anchor="ls", align="left", fill=text_color)
-            draw.text((980, 220), text="TRK", font=font, anchor="rs", align="right", fill=text_color)
+            draw.text((960, 220), text="TRK", font=font, anchor="rs", align="right", fill=text_color)
 
         if heading_mode:
-            draw.text((1100, 120), text="V/S", font=font, anchor="ls", align="left", fill=text_color)
-            draw.text((1900, h), text="V/S", font=font, anchor="rs", align="right", fill=text_color)
+            draw.text((1080, 120), text="V/S", font=font, anchor="ls", align="left", fill=text_color)
+            draw.text((1880, h), text="V/S", font=font, anchor="rs", align="right", fill=text_color)
         else:
-            draw.text((1100, 220), text="FPA", font=font, anchor="ls", align="left", fill=text_color)
+            draw.text((1080, 220), text="FPA", font=font, anchor="ls", align="left", fill=text_color)
             draw.text((8 * ICON_SIZE - inside, h), text="FPA", font=font, anchor="rs", align="right", fill=text_color)
 
-        draw.text((1340, h), text="ALT", font=font, anchor="ls", align="left", fill=text_color)
-        draw.text((1620, h), text="LVL/CH", font=font, anchor="ms", align="center", fill=text_color)
+        draw.text((1320, h), text="ALT", font=font, anchor="ls", align="left", fill=text_color)
+        draw.text((1600, h), text="LVL/CH", font=font, anchor="ms", align="center", fill=text_color)
 
         # line
         h = inside + text_size / 2 + 4
-        draw.line([(1430, h), (1530, h)], fill=text_color, width=3, joint="curve")
-        draw.line([(1430, h), (1430, h + text_size / 3)], fill=text_color, width=3, joint="curve")
-        draw.line([(1720, h), (1820, h)], fill=text_color, width=3, joint="curve")
-        draw.line([(1820, h), (1820, h + text_size / 3)], fill=text_color, width=3, joint="curve")
+        draw.line([(1410, h), (1510, h)], fill=text_color, width=3, joint="curve")
+        draw.line([(1410, h), (1410, h + text_size / 3)], fill=text_color, width=3, joint="curve")
+        draw.line([(1700, h), (1800, h)], fill=text_color, width=3, joint="curve")
+        draw.line([(1800, h), (1800, h + text_size / 3)], fill=text_color, width=3, joint="curve")
 
         # values
         # pylint: disable=W0612
@@ -151,33 +119,75 @@ class FCUIcon(DrawBase):
         font = self.get_font(text_font, text_size)
         one = " 1" if text_font == "Seven Segment" else "1"
         h = 200
-        draw.text((20, h), text=speed.replace("1", one), font=font, anchor="ls", align="left", fill=text_color)
-        draw.text((500, h), text=heading.replace("1", one), font=font, anchor="ls", align="left", fill=text_color)
-        draw.text((1260, h), text=alt.replace("1", one), font=font, anchor="ls", align="left", fill=text_color)  # should always be len=5
-        draw.text((1720, h), text=vs.replace("1", one), font=font, anchor="ls", align="left", fill=text_color)  # should always be len=5 or 6
+        dot_size = 24
+        hdot = 160
 
-        font = self.get_font("Seven Segment", int(0.7 * text_size))
-        if vs_val > 0:
-            vs = "+"
-        elif vs_val < 0:
-            vs = "-"
-        draw.text((1660, h - 16), text=vs, font=font, anchor="ls", align="left", fill=text_color)  # should always be len=5 or 6
-
-        # dots
-        dot_size = 20
-        h = 170
+        #
+        # SPEED
         if speed_managed:
+            speed = "---"
+            draw.text((20, h), text=speed, font=font, anchor="ls", align="left", fill=text_color)
             w = 250
-            dot = ((w - dot_size, h - dot_size), (w + dot_size, h + dot_size))
+            dot = ((w - dot_size, hdot - dot_size), (w + dot_size, hdot + dot_size))
             draw.ellipse(dot, fill=text_color)
+        else:
+            spdft = 0.56 if mach_mode else 249
+            speed_val = self.button.get_dataref_value_from_collection("sim/cockpit2/autopilot/airspeed_dial_kts_mach", FCU_COLLECTION, default=spdft)
+            speed = ""
+            if mach_mode:
+                speed_val = round(speed_val * 100) / 100
+                speed = f"{speed_val:4.2f}"
+            else:
+                speed_val = int(speed_val)
+                speed = f"{speed_val:3d}"
+            draw.text((20, h), text=speed.replace("1", one), font=font, anchor="ls", align="left", fill=text_color)
+        #
+        # HEADING
         if heading_managed:
-            w = 730
-            dot = ((w - dot_size, h - dot_size), (w + dot_size, h + dot_size))
+            heading = "---"
+            draw.text((500, h), text=heading, font=font, anchor="ls", align="left", fill=text_color)
+            w = 736
+            dot = ((w - dot_size, hdot - dot_size), (w + dot_size, hdot + dot_size))
             draw.ellipse(dot, fill=text_color)
+        else:
+            heading_val = self.button.get_dataref_value_from_collection("sim/cockpit/autopilot/heading_mag", FCU_COLLECTION, 0)
+            heading_val = int(heading_val)
+            heading = f"{heading_val:3d}"
+            draw.text((500, h), text=heading.replace("1", one), font=font, anchor="ls", align="left", fill=text_color)
+        #
+        # ALTITUDE (always displayed)
+        vs_dashed = self.button.get_dataref_value_from_collection("AirbusFBW/VSdashed", FCU_COLLECTION, False)
+        alt_ft_val = self.button.get_dataref_value_from_collection("sim/cockpit2/autopilot/altitude_dial_ft", FCU_COLLECTION, 26789)
+        alt_ft_val = int(alt_ft_val)
+        alt = f"{alt_ft_val: 5d}"
+        draw.text((1240, h), text=alt.replace("1", one), font=font, anchor="ls", align="left", fill=text_color)  # should always be len=5
         if alt_managed:
-            w = 1610
-            dot = ((w - dot_size, h - dot_size), (w + dot_size, h + dot_size))
+            w = 1590
+            dot = ((w - dot_size, hdot - dot_size), (w + dot_size, hdot + dot_size))
             draw.ellipse(dot, fill=text_color)
+
+        # Vertical speed/slope is tricky
+        vs_val = -1
+        if alt_managed or vs_dashed:
+            vs = "----" if heading_mode else "-.---"
+            draw.text((1700, h), text=vs.replace("1", one), font=font, anchor="ls", align="left", fill=text_color)  # should always be len=5 or 6
+        else:
+            vsdft = -1200 if heading_mode else -2.5
+            vs_val = self.button.get_dataref_value_from_collection("sim/cockpit/autopilot/vertical_velocity", FCU_COLLECTION, default=vsdft)
+            vs_val_abs = abs(vs_val)
+            vs = ""
+            if heading_mode:  # V/S
+                vs_val_abs = abs(int(vs_val / 100))
+                vs = f"{vs_val_abs:02d}" + "oo"  # little zeros
+            else:  # FPA
+                vs_val_abs = abs(round(vs_val * 10) / 10)
+                vs = f"{vs_val_abs:3.1f}"
+            # print(">>>", vs_val, heading_mode, alt_managed, vs)
+            draw.text((1700, h), text=vs.replace("1", one), font=font, anchor="ls", align="left", fill=text_color)  # should always be len=5 or 6
+        # little + or - in front of vertical speed
+        font = self.get_font("Seven Segment", int(0.7 * text_size))
+        vs = "-" if vs_val < 0 else "+"
+        draw.text((1650, h - 16), text=vs, font=font, anchor="ls", align="left", fill=text_color)  # should always be len=5 or 6
 
         # Paste image on cockpit background and return it.
         bg = self.button.deck.get_icon_background(
