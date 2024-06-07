c6d13ac2f77389bab551c9bbc72c816cd00098b2
Pierre M <pierre@devleaks.be>
2024-06-07 15:12:58 +0200

    template network file

--- cockpitdecks/network-template.py
+++ cockpitdecks/network-template.py
@@ -1,3 +1,3 @@
 COCKPITDECKS_HOST = ("127.0.0.1", 7700)  # That's where cockpitdecks receives updates from virtual decks
 PROXY_HOST = ["127.0.0.1", 7699]  # That's where cockpitdecks send information to web decks
-APP_HOST = ["mac-studio-de-pierre.local", 7777]  # That's where the web deck app run and can be contacted by the browser.
+APP_HOST = ["127.0.0.1", 7777]  # That's where the web deck app run and can be contacted by the browser.
